1c0b5180d47f87913cb8f447f97f126eb9d855c1
yaugesha <eugene.korchemenko@gmail.com>
2024-06-14 11:31:07 +0300

    feat: add asset management for pctures and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   resolve: {
