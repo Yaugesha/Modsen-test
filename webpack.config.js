a2300d14056283436733ed120076902b7aebc9de
yaugesha <eugene.korchemenko@gmail.com>
2024-06-25 19:11:40 +0300

    feat: add firebase config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -54,6 +55,7 @@ module.exports = {
       title: 'Modsen Shop',
       template: './public/index.html',
     }),
+    new Dotenv(),
   ],
   devServer: {
     static: {
